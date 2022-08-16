4b9132404ea53e854c6eec0dd3a4cc8e0ff6aa1a
kartikeya kapparwan <kartik.kapparwan1@gmail.com>
2022-08-16 08:42:09 +0530

    Add install db file

---
+++ database/install_db.sql
@@ -0,0 +1,42 @@
+drop database if exists `patient_registration`;
+
+create database `patient_registration`;
+
+use `patient_registration`;
+
+set FOREIGN_KEY_CHECKS=0;
+
+create table `patient_registration`.`hospitals` (
+    `hospital_id` int(11) NOT NULL AUTO_INCREMENT,
+    `hospital_name` varchar(255) NOT NULL,
+    PRIMARY KEY (`hospital_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table `patient_registration`.`psychiatrists` (
+    `psychiatrist_id` int(11) NOT NULL AUTO_INCREMENT,
+    `psychiatrist_name` varchar(255) NOT NULL,
+    `psychiatrist_hospital_id` int(11) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`psychiatrist_id`),
+    KEY `fk_psychiatrist_hospital_id` (`psychiatrist_hospital_id`),
+    CONSTRAINT `fk_psychiatrist_hospital_id` FOREIGN KEY (`psychiatrist_hospital_id`) REFERENCES `hospitals` (`hospital_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table `patient_registration`.`patients` (
+    `patient_id` int(11) NOT NULL AUTO_INCREMENT,
+    `patient_name` varchar(255) NOT NULL,
+    `patient_address` varchar(255) NOT NULL,
+    `patient_phone` varchar(255) NOT NULL,
+    `patient_email` varchar(255) NOT NULL,
+    `patient_password` varchar(255) NOT NULL,
+    `patient_photo` varchar(255) NOT NULL,
+    `patient_psychiatrist_id` int(11) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`patient_id`),
+    KEY `fk_patient_psychiatrist_id` (`patient_psychiatrist_id`),
+    CONSTRAINT `fk_patient_psychiatrist_id` FOREIGN KEY (`patient_psychiatrist_id`) REFERENCES `psychiatrists` (`psychiatrist_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+set FOREIGN_KEY_CHECKS=1;
