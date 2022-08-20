a7757abbdd973a90f41103c7b9fb1a539915d0b1
kartikeya kapparwan <kartik.kapparwan1@gmail.com>
2022-08-20 10:52:47 +0530

    Add hospital list to be inserted

--- database/install_db.sql
+++ database/install_db.sql
@@ -43,4 +43,10 @@ create table `patient_registration`.`patients` (
     CONSTRAINT `fk_patient_psychiatrist_id` FOREIGN KEY (`patient_psychiatrist_id`) REFERENCES `psychiatrists` (`psychiatrist_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `hospitals` (`hospital_name`) VALUES
+('Apollo Hospitals'),
+('Jawaharlal Nehru Medical College and Hospital'),
+('Indira Gandhi Institute of Medical Sciences (IGIMS)'),
+('AIIMS - All India Institute Of Medical Science');
+
 set FOREIGN_KEY_CHECKS=1;
